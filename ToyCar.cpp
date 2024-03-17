da5a7e98daf3de34d39a592f413781533b8dd5df
tif-car <68936978+tif-car@users.noreply.github.com>
2024-03-17 12:56:14 -0500

    Update ToyCar.cpp

--- ToyCar.cpp
+++ ToyCar.cpp
@@ -1,248 +1,49 @@
 #include <IRremote.h>
-//#include <HCSR04.h>    debug tc
-
-//HCSR04 hc(9, 8); //initialisation class HCSR04 (trig pin , echo pin)   debug tc
-
-int irPin = 7;
-IRrecv irRecive(irPin);
-decode_results results;
-
-// left motor
-int leftMotorSpeedPin = 3;
-int leftMotorForwardPin = 4;
-int leftMotorBackwardPin = 5;
-
-// right motor
-int rightMotorSpeedPin = 11;
-int rightMotorForwardPin = 12;   //purple problem tc
-int rightMotorBackwardPin = 13;
-
-int direction = 0; //0 = stop, 1 = forward, 2 = bakward
-
-
-void setup() {
-  pinMode(leftMotorSpeedPin, OUTPUT);
-  pinMode(leftMotorForwardPin, OUTPUT);
-  pinMode(leftMotorBackwardPin, OUTPUT);
-  pinMode(rightMotorSpeedPin, OUTPUT);
-  pinMode(rightMotorForwardPin, OUTPUT);
-  pinMode(rightMotorBackwardPin, OUTPUT);
-
-  digitalWrite(leftMotorSpeedPin, HIGH);
-  digitalWrite(rightMotorSpeedPin, HIGH);
-
-  //analogWrite(leftMotorSpeedPin, i);
-  //analogWrite(rightMotorSpeedPin, i);
-
-  Serial.begin(9600);      
-  //irRecive.enableIRIn();   //debug tc
-
-  //goForward();   //debug tc erase later
-  stop();
-  goBackward();  
-  //goForward();
-}
-
-void loop() {
-
-  //Serial.println(hc.dist()); // return curent distance in serial   debug tc
-  delay(60);                 // we suggest to use over 60ms measurement cycle, in order to prevent trigger signal to the echo signal.
-
-} //end of loop
-
-void goForward() {
-  Serial.println("goForward() function called");
-  digitalWrite(leftMotorForwardPin, HIGH);   
-  digitalWrite(leftMotorBackwardPin, LOW);    
-  digitalWrite(rightMotorForwardPin, HIGH);     
-  digitalWrite(rightMotorBackwardPin, LOW);    
-  direction = 1;
-}
-
-void goBackward() {
-  Serial.println("goBackward() function called");
-  digitalWrite(leftMotorForwardPin, LOW);
-  digitalWrite(leftMotorBackwardPin, HIGH);
-  digitalWrite(rightMotorForwardPin, LOW);
-  digitalWrite(rightMotorBackwardPin, HIGH);
-  direction = 2;
-}
-
-void goRight() {
-  Serial.println("goRight() function called");
-  digitalWrite(leftMotorForwardPin, HIGH);
-  digitalWrite(leftMotorBackwardPin, LOW);
-  digitalWrite(rightMotorForwardPin, LOW);
-  digitalWrite(rightMotorBackwardPin, LOW);
-}
-
-void goLeft() {
-  Serial.println("goLeft() function called");
-  digitalWrite(leftMotorForwardPin, LOW);
-  digitalWrite(leftMotorBackwardPin, LOW);
-  digitalWrite(rightMotorForwardPin, HIGH);
-  digitalWrite(rightMotorBackwardPin, LOW);
-}
-
-void stop() {
-  Serial.println("stop() function called");
-  digitalWrite(leftMotorForwardPin, LOW);
-  digitalWrite(leftMotorBackwardPin, LOW);
-  digitalWrite(rightMotorForwardPin, LOW);
-  digitalWrite(rightMotorBackwardPin, LOW);
-  direction = 0;
-}
-
-void continueForwardOrBackward() {
-  if (direction == 1) {
-    goForward();
-  } else if (direction == 2) {
-    goBackward();
-  } else {
-    stop();
-  }
-}
-/////////
-
-#include <IRremote.h>
+#include <DIYables_IRcontroller.h>
 #include <HCSR04.h>    debug tc  
 
 
-HCSR04 hc(9, 8); //initialisation class HCSR04 (trig pin , echo pin)   debug tc
+//HCSR04 hc(9, 8); //initialisation class HCSR04 (trig pin , echo pin)   debug tc
 
-
-/*
-int in1 = 2;
-int in2 = 4;
-int enA = 3;
-
-int in3 = 5;
-int in4 = 7;
-int enB = 6;
-*/
-
-/*
-
-//IRrecv irRecive(irPin);
+int irPin = 9;            //Arduino pin connected to IR receiver
+IRrecv irRecive(irPin);
 decode_results results;
 
-// left motor
-int leftMotorSpeedPin = 3;
-int leftMotorForwardPin = 4;
-int leftMotorBackwardPin = 2;
+//left motor
+int in1 = 6;
+int in2 = 5;
+int enA = 7;
 
-// right motor
-int rightMotorSpeedPin = 6;
-int rightMotorForwardPin = 7;   //purple problem tc
-int rightMotorBackwardPin = 5;
-
-int direction = 1; //0 = stop, 1 = forward, 2 = bakward
-
-
-
-void setup() {
-  pinMode(leftMotorSpeedPin, OUTPUT);
-  pinMode(leftMotorForwardPin, OUTPUT);
-  pinMode(leftMotorBackwardPin, OUTPUT);
-  pinMode(rightMotorSpeedPin, OUTPUT);
-  pinMode(rightMotorForwardPin, OUTPUT);
-  pinMode(rightMotorBackwardPin, OUTPUT);
-
-  digitalWrite(leftMotorSpeedPin, HIGH);
-  digitalWrite(rightMotorSpeedPin, HIGH);
+//right motor
+int in3 = 4;
+int in4 = 3;
+int enB = 2;
 
   //analogWrite(leftMotorSpeedPin, i);
   //analogWrite(rightMotorSpeedPin, i);
 
-  Serial.begin(9600);      
-  //irRecive.enableIRIn();   //debug tc
-
-  goForward();   //debug tc erase later
-  stop();
-  goBackward();  
-  goForward();
-  
-
-
-}
-
-void loop() {
-
-  Serial.println(hc.dist()); // return curent distance in serial    debug tc
-  delay(5);                 // we suggest to use over 60ms measurement cycle, in order to prevent trigger signal to the echo signal.
-
-
-
-
-
-} //end of loop
-
-void goForward() {
-  Serial.println("goForward() function called");
-  digitalWrite(leftMotorForwardPin, HIGH);   
-  digitalWrite(leftMotorBackwardPin, LOW);    
-  digitalWrite(rightMotorForwardPin, HIGH);     
-  digitalWrite(rightMotorBackwardPin, LOW);    
-  direction = 1;
-}
-
-void goBackward() {
-  Serial.println("goBackward() function called");
-  digitalWrite(leftMotorForwardPin, LOW);
-  digitalWrite(leftMotorBackwardPin, HIGH);
-  digitalWrite(rightMotorForwardPin, LOW);
-  digitalWrite(rightMotorBackwardPin, HIGH);
-  direction = 2;
-}
-
-void goRight() {
-  Serial.println("goRight() function called");
-  digitalWrite(leftMotorForwardPin, HIGH);
-  digitalWrite(leftMotorBackwardPin, LOW);
-  digitalWrite(rightMotorForwardPin, LOW);
-  digitalWrite(rightMotorBackwardPin, LOW);
-}
-
-void goLeft() {
-  Serial.println("goLeft() function called");
-  digitalWrite(leftMotorForwardPin, LOW);
-  digitalWrite(leftMotorBackwardPin, LOW);
-  digitalWrite(rightMotorForwardPin, HIGH);
-  digitalWrite(rightMotorBackwardPin, LOW);
-}
-
-void stop() {
-  Serial.println("stop() function called");
-  digitalWrite(leftMotorForwardPin, LOW);
-  digitalWrite(leftMotorBackwardPin, LOW);
-  digitalWrite(rightMotorForwardPin, LOW);
-  digitalWrite(rightMotorBackwardPin, LOW);
-  direction = 0;
-}
-
-void continueForwardOrBackward() {
-  if (direction == 1) {
-    goForward();
-  } else if (direction == 2) {
-    goBackward();
-  } else {
-    stop();
-  }
-}
-
-
-
-*/
-/*
 void setup() {
+  Serial.begin(9600);      
+  irRecive.enableIRIn();   //debug tc
+
   pinMode(enA, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(enB, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
+
+  digitalWrite(enA, HIGH);  //for full speed
+  digitalWrite(enB, HIGH);  //for full speed
+  
+  goForward();   //debug tc erase later
+  stop();
+  goBackward();  
+  goForward();
+
 }
 
+//for testing
 void loop() {
   // Move forward for 1.5 seconds at full speed
   digitalWrite(in1, HIGH);
@@ -273,9 +74,22 @@ void loop() {
   delay(500);
 }
 
+//testing forward/backward
+void continueForwardOrBackward() {
+  if (direction == 1) {
+    goForward();
+  } else if (direction == 2) {
+    goBackward();
+  } else {
+    stop();
+  }
+}
 
 
-/*
+
+/*  
+////for testing motors and pins//////
+
 void setup(){
   pinMode(enA, OUTPUT);
   pinMode(in1, OUTPUT);
@@ -368,54 +182,5 @@ void loop(){
  return 0;
 
 }
+//////testing over//////
 */
-
-// Define the motor pins
-int in1 = 2;
-int in2 = 4;
-int enA = 3;
-
-int in3 = 5;
-int in4 = 7;
-int enB = 6;
-
-void setup() {
-  // Set motor pins as OUTPUT
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(enA, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
-  pinMode(enB, OUTPUT);
-}
-
-void loop() {
-  // Move forward for 3 seconds
-  moveForward();
-  delay(3000);
-
-  // Move backward for 3 seconds
-  moveBackward();
-  delay(3000);
-}
-
-void moveForward() {
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  digitalWrite(enA, 255);  // Assuming enA is a PWM pin, setting it to maximum value for full speed
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
-  digitalWrite(enB, 255);  // Assuming enB is a PWM pin, setting it to maximum value for full speed
-}
-
-void moveBackward() {
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  digitalWrite(enA, 255);  // Assuming enA is a PWM pin, setting it to maximum value for full speed
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
-  digitalWrite(enB, 255);  // Assuming enB is a PWM pin, setting it to maximum value for full speed
-}
-
-
-
